ba3df52ccd1be74697cb881d8c3a32d9bdb0dc5d
Jihye Kim <wlgp115@gmail.com>
2024-05-30 15:19:38 +0900

    [쌤코드] 싫어요 버튼 구현(삼항조건연산자)

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  // 좋아요
   const [like, setLike] = useState(false);
   const onClick = () => setLike((prev) => !prev);
+
+  // 싫어요
+  const [dislike, setDislike] = useState(false);
+  const onClickDis = () => setDislike((prev) => !prev);
 
   return (
     <>
@@ -15,6 +20,15 @@ function App() {
         <button onClick={onClick}>좋아요 취소</button>
       ) : (
         <button onClick={onClick}>좋아요</button>
+      )}
+
+      <span> </span>
+
+      {/* 싫어요 버튼 */}
+      {dislike ? (
+        <button onClick={onClickDis}>싫어요 취소</button>
+      ) : (
+        <button onClick={onClickDis}>싫어요</button>
       )}
     </>
   );
