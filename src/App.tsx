bf79f4c41675be0d70c15ffdeba7f0e03d4977ac
Jihye Kim <wlgp115@gmail.com>
2024-05-30 13:14:36 +0900

    싫어요 버튼 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,24 @@ import "./App.css";
 
 function App() {
   const [like, setLike] = useState(false);
+  const [dislike, setDislike] = useState(false);
 
   return (
     <>
+      {/* 좋아요 버튼 */}
       {like === false ? (
         <button onClick={() => setLike(true)}>좋아요</button>
       ) : (
         <button onClick={() => setLike(false)}>좋아요 취소</button>
+      )}
+
+      <span> </span>
+      
+      {/* 실어요 버튼 */}
+      {dislike === false ? (
+        <button onClick={() => setDislike(true)}>싫어요</button>
+      ) : (
+        <button onClick={() => setDislike(false)}>싫어요 취소</button>
       )}
     </>
   );
